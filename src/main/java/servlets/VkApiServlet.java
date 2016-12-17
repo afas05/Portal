691d7d324a6fc7f66e0568ac42b9b474f2ebb6e3
Ihor <afas05@mail.ru>
2016-12-17 13:53:55 +0200

    Update VkApiServlet.java

--- src/main/java/servlets/VkApiServlet.java
+++ src/main/java/servlets/VkApiServlet.java
@@ -17,7 +17,7 @@ public class VkApiServlet {
     private VkApiClient apiClient;
     private UserActor actor;
     private int appId = 17508531;
-    private String token = "7b168496c726d181c55bf189fc40b652c7620dbf7593955ceaea07522e41dc6ab2ed8350bfd4645a0f8df";
+    private String token = "my token";
 
     public VkApiServlet() {
         TransportClient transportClient = HttpTransportClient.getInstance();
